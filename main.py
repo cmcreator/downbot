9869efcf0717585f178034063774fe37afb04124
Chumachenko Kirill <chumachenko936@gmail.com>
2020-04-26 00:12:14 +0300

    fix bug with '?' in file_name

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import os
+import re
 from pprint import pprint
 
 import youtube_dl
-
-#
-# ydl_opts = {
-#     'format': 'bestaudio/best',
-#     'postprocessors': [{
-#         'key': 'FFmpegExtractAudio',
-#         'preferredcodec': 'mp3',
-#         'preferredquality': '192',
-#     }],
-# }
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     meta=ydl.extract_info('https://www.youtube.com/watch?v=6BuJMvpAaDA',download=True)
-#
-# pprint(meta)
-def chek(info):
-    pprint(info)
 
 
 class Downloader:
@@ -27,32 +12,45 @@ class Downloader:
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
+
         }],
+        'outtmpl': '%(title)s-%(id)s.%(ext)s',
     }
+
     def __init__(self):
-        self.name_file=None
+        self.name_file = None
+
+    def run(self, url, param=None):
+        info = self._get_and_download(url)
+        self.name_file = info['title'] + '-' + info['id'] + '.mp3'
+        self.refacor()
 
 
-    def run(self,url,param=None):
-        info=self._get_and_download(url)
-        self.name_file=info['title']+'-'+info['id']
-        print(self.name_file)
-        # chek(info)
+    def refacor(self):
+        '''
+                        Убираем ? символ из .mp3
+        '''
+        reg = re.compile('[?]')
+        self.name_file=reg.sub('', self.name_file)
+
 
 
     def _get_and_download(self, url):
+        '''
+                Скачивание .mp3 и получение метаданных
+        '''
         with youtube_dl.YoutubeDL(Downloader.ydl_opts) as ydl:
             meta = ydl.extract_info(url, download=True)
         return meta
 
     def del_file(self):
-        self.name_file=self.name_file+'.mp3'
-        path=os.path.join(os.getcwd(),self.name_file)
+        '''
+        Удаление .mp3
+        '''
+        path = os.path.join(os.getcwd(), self.name_file)
         os.remove(self.name_file)
 
 
-
-video_url=Downloader()
+video_url = Downloader()
 video_url.run(str(input('input URL:')))
-video_url.del_file()
-
+# video_url.del_file()
